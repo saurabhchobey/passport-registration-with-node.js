0b1bb4c762505deafc43562181b260b36c3863f7
saurabhchobey <52131942+saurabhchobey@users.noreply.github.com>
2021-02-18 14:38:56 +0530

    Update keys.js

--- config/keys.js
+++ config/keys.js
@@ -4,23 +4,3 @@ module.exports = {
     mongoURI: dbPassword
 };
 
-// const mongoose = require("mongoose");
-
-// function dbConfig() {
-//     /* mongoose
-//         .connect("mongodb+srv://Jyoti123:Priya123@cluster0.xmwft.mongodb.net/Jyoti?retryWrites=true&w=majority", {
-//             useNewUrlParser: true
-//         }) */
-//         mongoose
-//         .connect('mongodb://localhost:27017/bluehills',{
-//             useNewUrlParser: true,
-//             useUnifiedTopology: true
-//         })
-        
-//         .then(() => console.log("connected to MongoDb"))
-//         .catch(err => console.log("could not connect to mongodb" + err));
-
-// }
-
-// //exporting the connection and configuration
-// module.exports.dbConfig = dbConfig;
